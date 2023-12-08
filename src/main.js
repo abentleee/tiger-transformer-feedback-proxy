b7643f5bc8d2e272729fec52cb3bb5b060ceb33e
abentleee <alanbentleypgh@gmail.com>
2023-12-08 10:41:35 -0500

    implement error endpoint

--- src/main.js
+++ src/main.js
@@ -10,9 +10,10 @@ app.use(express.text());
 
 const port = process.env.PORT || 3000;
 
+// success endpoint
 app.post('/api/v1/success', async (req, res) => {
     const host = `${process.env.TIGER_TRANSFORMER_FEEDBACK_API_HOST}/api/v1/success`;
-    
+
     // pull creds from request body (temp workaround due to cors issue)
     let body = req.body;
     const auth = body.substring(body.indexOf(`Basic`), body.indexOf(`\"}`))
@@ -37,7 +38,46 @@ app.post('/api/v1/success', async (req, res) => {
         body,
     }
 
-    console.log(`host: ${host}, options: ${JSON.stringify(options)}`);
+    try {
+        const response = await fetch(host, options);
+        console.log(`response: ${JSON.stringify(response.status)} - ${JSON.stringify(response)}`);
+
+        res.status(201).send();
+    } catch (err) {
+        console.error(err);
+        res.status(500).send();
+    }
+});
+
+// error endpoint
+app.post('/api/v1/error', async (req, res) => {
+    const host = `${process.env.TIGER_TRANSFORMER_FEEDBACK_API_HOST}/api/v1/error`;
+
+    // pull creds from request body (temp workaround due to cors issue)
+    let body = req.body;
+    const auth = body.substring(body.indexOf(`Basic`), body.indexOf(`\"}`))
+
+    console.log(`auth: ${auth}`)
+
+    // remove auth from payload to api
+    body = body.replace(`, "authorization": "${auth}"`, '');
+
+    // return bad request if no request body
+    if (!Object.keys(req.body).length) {
+        console.error(`no request body found, returning 400`);
+        res.status(400).send();
+        return;
+    }
+
+    // call tiger-transformer-feedback-api app with headers and body
+    const options = {
+        method: 'POST',
+        headers: {
+            'Authorization': auth,
+            'Content-Type': 'application/json'
+        },
+        body,
+    }
 
     try {
         const response = await fetch(host, options);
@@ -48,9 +88,9 @@ app.post('/api/v1/success', async (req, res) => {
         console.error(err);
         res.status(500).send();
     }
-
-    res.status(201).send();
 });
+
+// TODO: feedback endpoint
 
 app.get('/', (req, res) => {
     res.send('Hello world!');
